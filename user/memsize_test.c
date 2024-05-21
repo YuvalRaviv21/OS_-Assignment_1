3935175f0392d5dc39523a0119f13ebdc6ae1c87
Yuval Raviv <yuvalraviv21@gmail.com>
2024-05-21 07:57:20 +0000

    Task2

---
+++ user/memsize_test.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+#define SIZE_20K 20000
+
+int
+main(int argc, char *argv[])
+{
+    printf("Initial memory usage: %d bytes\n", memsize());
+    char *ptr = malloc(SIZE_20K);
+    printf("Memory usage after allocation: %d bytes\n", memsize());
+    free(ptr);
+    printf("Memory usage after release: %d bytes\n", memsize());
+    exit(0);
+}
+
+// TODO 2.5
