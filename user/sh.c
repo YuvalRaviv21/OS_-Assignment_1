f1efa0520e0d8e02b9702b6de3634e8847637e68
Yuval Raviv <yuvalraviv21@gmail.com>
2024-06-04 21:44:47 +0000

    Add function

--- user/sh.c
+++ user/sh.c
@@ -94,7 +94,7 @@ runcmd(struct cmd *cmd)
     lcmd = (struct listcmd*)cmd;
     if(fork1() == 0)
       runcmd(lcmd->left);
-    wait(0);
+    wait(0,0);
     runcmd(lcmd->right);
     break;
 
@@ -118,8 +118,8 @@ runcmd(struct cmd *cmd)
     }
     close(p[0]);
     close(p[1]);
-    wait(0);
-    wait(0);
+    wait(0,0);
+    wait(0,0);
     break;
 
   case BACK:
